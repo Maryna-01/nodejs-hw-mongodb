f17d4eeeb631ab4ef34766e4d7b6d88ff5a4e725
Maryna Liashchenko <127967599+Maryna-01@users.noreply.github.com>
2025-01-07 13:30:12 +0100

    add changes

--- src/services/contacts.js
+++ src/services/contacts.js
@@ -4,11 +4,11 @@ import {
 from '../models/contact.js';
 
 export const getAllContacts = async () => {
-    const result = await getAllContacts.find({}, '-createdAt -updatedAt');
+    const result = await Contact.find();
     return result;
 };
 export const getContactById = async (contactId) => {
-    const result = await Contact.findById(contactId, '-createdAt -updatedAt');
+    const result = await Contact.findById(contactId);
     return result;
 };
 
