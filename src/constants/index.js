eee7b1b7fb28b10b381cfbf9342b9198b7b09082
Maryna Liashchenko <127967599+Maryna-01@users.noreply.github.com>
2025-01-27 13:32:51 +0100

    add changes

--- src/constants/index.js
+++ src/constants/index.js
@@ -10,22 +10,21 @@ export const FIFTEEN_MINUTES = 15 * 60 * 1000;
 export const THIRTY_DAYS = 30 * 24 * 60 * 60 * 1000;
 
 export const SMTP = {
-  SMTP_HOST: 'SMTP_HOST',
-  SMTP_PORT: 'SMTP_PORT',
-  SMTP_USER: 'SMTP_USER',
-  SMTP_PASSWORD: 'SMTP_PASSWORD',
-  SMTP_FROM: 'SMTP_FROM',
+  HOST: 'SMTP_HOST',
+  PORT: 'SMTP_PORT',
+  USER: 'SMTP_USER',
+  PASSWORD: 'SMTP_PASSWORD',
+  FROM: 'SMTP_FROM',
 };
 
-export const TEMPLATES_DIR = path.resolve('src', 'templates');
-
-export const TEMP_UPLOAD_DIR = path.resolve('temp');
-export const UPLOAD_DIR = path.resolve('uploads');
+export const TEMPLATES_DIR = path.join(process.cwd(), 'src', 'templates');
+export const TEMP_UPLOAD_DIR = path.join(process.cwd(), 'temp');
+export const UPLOAD_DIR = path.join(process.cwd(), 'uploads');
 
 export const CLOUDINARY = {
-  CLOUD_NAME: 'CLOUD_NAME',
-  CLOUD_API_KEY: 'CLOUD_API_KEY',
-  CLOUD_API_SECRET: 'CLOUD_API_SECRET',
-}
+  NAME: 'CLOUDINARY_CLOUD_NAME',
+  API_KEY: 'CLOUDINARY_CLOUD_API_KEY',
+  API_SECRET: 'CLOUDINARY_CLOUD_API_SECRET',
+};
 
 export const SWAGGER_PATH = path.resolve('docs', 'swagger.json');
--- src/utils/cloudinaryServices.js
+++ src/utils/cloudinaryServices.js
@@ -1,21 +1,29 @@
 import { v2 as cloudinary } from 'cloudinary';
 import fs from 'node:fs/promises';
+import dotenv from 'dotenv';
+dotenv.config();
 
-import { getEnvVar } from './getEnvVar.js';
-import { CLOUDINARY } from '../constants/index.js';
+
+export const CLOUDINARY = {
+  NAME: process.env.CLOUDINARY_CLOUD_NAME,
+  API_KEY: process.env.CLOUDINARY_CLOUD_API_KEY,
+  API_SECRET: process.env.CLOUDINARY_CLOUD_API_SECRET,
+};
 
 cloudinary.config({
   secure: true,
-  cloud_name: getEnvVar(CLOUDINARY.CLOUD_NAME),
-  api_key: getEnvVar(CLOUDINARY.CLOUD_API_KEY),
-  api_secret: getEnvVar(CLOUDINARY.CLOUD_API_SECRET),
+  cloud_name: CLOUDINARY.NAME,
+  api_key: CLOUDINARY.API_KEY,
+  api_secret: CLOUDINARY.API_SECRET,
 });
+
 
 export const saveFileToCloudinary = async (file) => {
   const response = await cloudinary.uploader.upload(file.path);
   await fs.unlink(file.path);
   return response;
 };
+
 
 export const deleteFileFromCloudinary = async (publicId) => {
   const response = await cloudinary.uploader.destroy(publicId);
