3098d8e5b11eef5a6164374508247b9a2bef0d1b
Maryna Liashchenko <127967599+Maryna-01@users.noreply.github.com>
2025-02-07 14:25:51 +0100

    add changes sendmail

--- src/utils/sendMail.js
+++ src/utils/sendMail.js
@@ -1,7 +1,9 @@
-import nodemailer from 'nodemailer';
-
-import { SMTP } from '../constants/index.js';
-import { getEnvVar } from './getEnvVar.js';
+import nodemailer from "nodemailer";
+import fs from "fs";
+import path from "path";
+import Handlebars from "handlebars";
+import { SMTP } from "../constants/index.js";
+import { getEnvVar } from "./getEnvVar.js";
 
 const transporter = nodemailer.createTransport({
   host: getEnvVar(SMTP.SMTP_HOST),
@@ -12,6 +14,33 @@ const transporter = nodemailer.createTransport({
   },
 });
 
-export const sendEmail = async (options) => {
-  return await transporter.sendMail(options);
+const loadTemplate = (templateName, data) => {
+  try {
+
+    const templatePath = path.resolve("src/templates", templateName);
+ 
+    
+
+    const templateSource = fs.readFileSync(templatePath, "utf8");
+
+    const template = Handlebars.compile(templateSource);
+    
+    return template(data);
+  } catch (error) {
+    console.error("Error loading email template:", error);
+    throw new Error("Email template not found");
+  }
+};
+
+export const sendEmail = async (to, subject, templateName, templateData) => {
+  const html = loadTemplate(templateName, templateData);
+
+  const mailOptions = {
+    from: `"Your App" <${getEnvVar(SMTP.SMTP_USER)}>`,
+    to,
+    subject,
+    html,
+  };
+
+  return await transporter.sendMail(mailOptions);
 };
