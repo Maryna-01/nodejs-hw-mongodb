a8722f31af21f8965deabccba64a795806ff16dc
Maryna Liashchenko <127967599+Maryna-01@users.noreply.github.com>
2025-02-08 00:51:03 +0100

    abd index and sandmail

--- src/utils/sendMail.js
+++ src/utils/sendMail.js
@@ -1,46 +1,17 @@
-import nodemailer from "nodemailer";
-import fs from "fs";
-import path from "path";
-import Handlebars from "handlebars";
-import { SMTP } from "../constants/index.js";
-import { getEnvVar } from "./getEnvVar.js";
+import nodemailer from 'nodemailer';
+
+import { SMTP } from '../constants/index.js';
+import { getEnvVar } from '../utils/getEnvVar.js';
 
 const transporter = nodemailer.createTransport({
-  host: getEnvVar("SMTP_HOST"),
-  port: Number(getEnvVar("SMTP_PORT")),
+  host: getEnvVar(SMTP.SMTP_HOST),
+  port: Number(getEnvVar(SMTP.SMTP_PORT)),
   auth: {
-    user: getEnvVar("SMTP_USER"),
-    pass: getEnvVar("SMTP_PASSWORD"),
+    user: getEnvVar(SMTP.SMTP_USER),
+    pass: getEnvVar(SMTP.SMTP_PASSWORD),
   },
 });
 
-const loadTemplate = (templateName, data) => {
-  try {
-
-    const templatePath = path.resolve("src/templates", templateName);
- 
-    
-
-    const templateSource = fs.readFileSync(templatePath, "utf8");
-
-    const template = Handlebars.compile(templateSource);
-    
-    return template(data);
-  } catch (error) {
-    console.error("Error loading email template:", error);
-    throw new Error("Email template not found");
-  }
-};
-
-export const sendEmail = async (to, subject, templateName, templateData) => {
-  const html = loadTemplate(templateName, templateData);
-
-  const mailOptions = {
-    from: `"Contact App" <${getEnvVar(SMTP_FROM)}>`,
-    to,
-    subject,
-    html,
-  };
-
-  return await transporter.sendMail(mailOptions);
+export const sendEmail = async (options) => {
+  return await transporter.sendMail(options);
 };
