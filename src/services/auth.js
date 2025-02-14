11932506c012aae2cd35cfdd7a3c2b2c563f2fae
Maryna Liashchenko <127967599+Maryna-01@users.noreply.github.com>
2025-02-14 15:58:21 +0100

    add abdate files post

--- src/services/auth.js
+++ src/services/auth.js
@@ -26,12 +26,15 @@ const createSession = () => {
 };
 ////////////////////////////////////////////////////////////////////
 export const register = async (payload) => {
-  //
+
+  console.log('Email from request:', payload.email); 
+
   const user = await UsersCollection.findOne({ email: payload.email });
   if (user) {
+    console.log('User not found in DB');
     throw createHttpError(409, 'Email in use');
   }
-  //
+  console.log('Found user:', user); 
   const hashPassword = await bcrypt.hash(payload.password, 10);
   //
   return await UsersCollection.create({ ...payload, password: hashPassword });
@@ -46,6 +49,7 @@ export const login = async (payload) => {
   //
   const passwordCompare = await bcrypt.compare(payload.password, user.password);
   if (!passwordCompare) {
+    console.log('Password does not match');
     throw createHttpError(401, 'Please check your password');
   }
   //
